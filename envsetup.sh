8f8450daeb8b8efeb6f08bc4123b5aebcb332579
erichu30 <erichu211@icloud.com>
2022-01-10 05:49:29 +0800

    update

--- envsetup.sh
+++ envsetup.sh
@@ -1,9 +1,9 @@
 #/bin/bash
-if hash conda 2>/dev/null; then
+if hash conda 2>/dev/null
 then
-    echo conda is not installed
-else
     echo conda is installed
     conda info --envs
     source activate DjongoWeb-3_9_7
+else
+    echo please install conda
 fi
